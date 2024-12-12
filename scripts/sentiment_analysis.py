36f3e48e7470bec12a968af01421e3abee2625ad
DomBonMar <domenico.bonillamarcello@mail.mcgill.ca>
2024-12-12 15:55:03 -0500

    Update sentiment_analysis.py
    Added title sentiment separately.

--- scripts/sentiment_analysis.py
+++ scripts/sentiment_analysis.py
@@ -5,6 +5,24 @@ def load_sentiment_data():
 
   return pd.read_csv("Sentiment_Data.csv")
 
+def score_sentence(df,sent):
+
+  valence = 0
+  intensity = 0
+
+  for w in sent:
+    # looks for entry in database
+    target = df[df['Word']==w]
+    if len(target) == 0:
+      continue
+
+    # else, computes sentiment scores for new word
+    valence += (target['Valence']).mean()
+    intensity += (target['Intensity']).mean()
+
+  return valence, intensity
+    
+
 def score_sentiment(dataset):
 
   # preprocess dataset
@@ -19,23 +37,28 @@ def score_sentiment(dataset):
   # assume text is encoded as a list of words
   valence_scores = np.zeros(len(dataset))
   intensity_scores = np.zeros(len(dataset))
+  title_valence = np.zeros(len(dataset))
+  title_intensity = np.zeros(len(dataset))
   
   for i,entry in enumerate(dataset):
-    
-    sent = entry.split(" ")
-    
-    for w in entry:
 
-      # looks for entry in database
-      target = df[df['Word']==w]
-      if len(target) == 0:
-        continue
+    # add code here to split data entry as title vs text
+    title = entry[0]
+    text = entry[1]
 
-      # else, computes sentiment scores for new word
-      valence_scores[i] += target['Valence']
-      intensity_scores[i] += target['Intensity']
+    # scores main text
+    sent = text.split(" ")
+    valence,intensity = score_sentence(sent)
+    valence_scores[i] = valence
+    intensity_scores[i] = intensity
 
-  return valence_scores, intensity_scores
+    # scores title
+    sent = title.split(" ")
+    valence,intensity = score_sentence(sent)
+    title_valence[i] = valence
+    title_intensity[i] = intensity
+
+  return valence_scores, intensity_scores, title_valence, title_intensity
 
     
 
