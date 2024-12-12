ecf4862f8b86e0810ef8f000dcc0d669c69afc35
DomBonMar <domenico.bonillamarcello@mail.mcgill.ca>
2024-12-12 14:29:09 -0500

    Create sentiment_analysis.py
    In progress [#1]

---
+++ scripts/sentiment_analysis.py
@@ -0,0 +1,13 @@
+def score_sentiment(text):
+
+  # given a bank of sentiment data, scores a piece of text
+  # attributes a valence score and an intensity score
+
+  # assume text is encoded as a list of words
+  valence_score = 0
+  intensity_score = 0
+  for w in text:
+
+    
+
+    
