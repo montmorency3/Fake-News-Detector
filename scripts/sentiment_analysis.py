6a26651c23d76c07e6953773a4d9061f0fce871c
DomBonMar <domenico.bonillamarcello@mail.mcgill.ca>
2024-12-12 16:14:39 -0500

    Create sentiment_analysis.py
    Re-added sentiment analysis.

---
+++ scripts/sentiment_analysis.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+import processData
+
+def load_sentiment_data():
+
+  return pd.read_csv("Sentiment_Data.csv")
+
+def score_sentence(df,sent):
+  valence = 0
+  intensity = 0
+  for w in sent:
+    # looks for entry in database
+    target = df[df['Word']==w]
+    if len(target) == 0:
+      continue
+    # else, computes sentiment scores for new word
+    valence += (target['Valence']).mean()
+    intensity += (target['Intensity']).mean()
+  return valence, intensity
+    
+def score_sentiment(dataset):
+
+  # assume text is encoded as a list of words
+  valence_scores = np.zeros(len(dataset))
+  intensity_scores = np.zeros(len(dataset))
+  title_valence = np.zeros(len(dataset))
+  title_intensity = np.zeros(len(dataset))
+
+  for i,entry in enumerate(dataset):
+
+    # add code here to split data entry as title vs text
+    title = entry[0]
+    text = entry[1]
+    
+    # scores main text
+    sent = text.split(" ")
+    valence,intensity = score_sentence(sent)
+    valence_scores[i] = valence
+    intensity_scores[i] = intensity
+
+    # scores title
+    sent = title.split(" ")
+    valence,intensity = score_sentence(sent)
+    title_valence[i] = valence
+    title_intensity[i] = intensity
+
+  return valence_scores, intensity_scores, title_valence, title_intensity
